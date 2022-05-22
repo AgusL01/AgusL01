26b3225017634dab7e31693770d48b96cfd97a7b
agusl01 <agustinlago908@gmail.com>
2022-05-22 18:38:57 -0300

    servido

---
+++ .history/404_20220522175556.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+</head>
+<body>
+.number 404
+.text
+    span Ooops...
+    <br>page not found
+|
+|
+a.me(href='https://codepen.io/uzcho_/pens/popular/?grid_type=list' target='_blank')
+</body>
+</html>
